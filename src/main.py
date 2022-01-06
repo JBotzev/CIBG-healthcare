cc88fee5c8f2b9e8c836c63debc8ef7dafae7b95
Joan Botzev <jbotzev@gmail.com>
2022-01-06 14:07:56 +0100

    MLR 75% acc output - before FS

--- src/main.py
+++ src/main.py
@@ -10,53 +10,37 @@ from Models.MLR import MLR
 
 def main():
 
-    df_csv = pd.read_csv('src\\res\\largeDataCategorical.csv')
+    print("Prepare Data . . .")
+    df_csv = pd.read_csv('res/largeDataCategorical.csv')
     print(df_csv)
 
     X = df_csv['X']
     Y = df_csv['Y']
     
-    x = []
-    y = []
+    X_new = []
     for ar in X:
         st = ar[1:-1]
         arr = st.split(',')
         arr = list(map(float, arr))
-        x.append(arr)
-    for ar in Y:
-        st = ar[1:-1]
-        arr = st.split(',')
-        arr = list(map(float, arr))
-        y.append(arr)
-
-    
-    
-
-
-    #X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size = 0.20, random_state = 5)
-
-    model = LogisticRegression(multi_class='multinomial', solver='lbfgs')
-    # model.fit(X_train, y_train)
-
-    #model.fit(X_train,y_train)
-    #y_pred = model.predict(X_test)
-    #print('Accuracy: {:.2f}'.format(accuracy_score(y_test, y_pred)))
-    #print('Error rate: {:.2f}'.format(1 - accuracy_score(y_test, y_pred)))
-
-
+        X_new.append(arr)
 
     print("Feature Selection . . .")
 
 
     print("\nMultinomial Logistic Regression . . .")
 
-    #TODO - pass real data
+    X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X_new, Y, test_size = 0.20, random_state = 5)
+
+    model = LogisticRegression(multi_class='multinomial', solver='lbfgs', max_iter=1500)
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    print('Accuracy: {:.2f}'.format(accuracy_score(y_test, y_pred)))
+    print('Error rate: {:.2f}'.format(1 - accuracy_score(y_test, y_pred)))
 
     # mlr = MLR()
     # mlr.train(X, Y)
     # mlr.evaluate()
     # mlr.predict()
-
 
     print("\nOther Methods . . .")
 
