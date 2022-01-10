d43f6d944485c9b9f117f3b374bc4df7a830234a
DanielvdVelde <danielvdvelde21@gmail.com>
2022-01-10 14:56:32 +0100

    ordinal regres (not workin)

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import sklearn
 from sklearn.linear_model import LogisticRegression
@@ -8,15 +9,20 @@ from sklearn.preprocessing import MultiLabelBinarizer
 
 from Models.MLR import MLR
 
-def main():
 
+from statsmodels.miscmodels.ordinal_model import OrderedModel # pip install git+https://github.com/statsmodels/statsmodels
+
+MODEL = 'ordinal' #'multinomial'
+
+
+def main():
     print("Prepare Data . . .")
     df_csv = pd.read_csv('res/largeDataCategorical.csv')
     print(df_csv)
 
     X = df_csv['X']
     Y = df_csv['Y']
-    
+
     X_new = []
     for ar in X:
         st = ar[1:-1]
@@ -26,16 +32,27 @@ def main():
 
     print("Feature Selection . . .")
 
-
     print("\nMultinomial Logistic Regression . . .")
 
-    X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X_new, Y, test_size = 0.20, random_state = 5)
+    X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X_new, Y, test_size=0.20, random_state=5)
 
-    model = LogisticRegression(multi_class='multinomial', solver='lbfgs', max_iter=1500)
-    model.fit(X_train, y_train)
-    y_pred = model.predict(X_test)
-    print('Accuracy: {:.2f}'.format(accuracy_score(y_test, y_pred)))
-    print('Error rate: {:.2f}'.format(1 - accuracy_score(y_test, y_pred)))
+    if MODEL == 'ordinal':
+        X_train = pd.DataFrame(X_train, dtype="object")
+        y_train = pd.DataFrame(y_train, dtype="category")
+
+        ordinalModel = OrderedModel(endog=y_train, exog=X_train, distr='logit')
+        res_log = ordinalModel.fit(method='bfgs', disp=True)
+        res_log.summary()
+        y_pred = res_log.predict(X_test)
+        print('Accuracy: {:.2f}'.format(accuracy_score(y_test, y_pred)))
+        print('Error rate: {:.2f}'.format(1 - accuracy_score(y_test, y_pred)))
+
+    if MODEL == 'multinomial':
+        model = LogisticRegression(multi_class='multinomial', solver='lbfgs', max_iter=1500)
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+        print('Accuracy: {:.2f}'.format(accuracy_score(y_test, y_pred)))
+        print('Error rate: {:.2f}'.format(1 - accuracy_score(y_test, y_pred)))
 
     # mlr = MLR()
     # mlr.train(X, Y)
