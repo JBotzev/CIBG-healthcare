0f9c5368da18e7e7f3c4a6fde453d2b8da998833
DanielvdVelde <danielvdvelde21@gmail.com>
2022-01-10 20:22:28 +0100

    Chino Chinitooo
    Jacarandosa
    Y mira como baila jacarandosa
    Y mira como goza jacarandosa
    Y mira como canta jacarandosa
    Y mira que sabrosa jacarandosa

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import numpy
 import numpy as np
 import pandas as pd
 import sklearn
@@ -9,10 +10,10 @@ from sklearn.preprocessing import MultiLabelBinarizer
 
 from Models.MLR import MLR
 
+from statsmodels.miscmodels.ordinal_model import \
+    OrderedModel  # pip install git+https://github.com/statsmodels/statsmodels
 
-from statsmodels.miscmodels.ordinal_model import OrderedModel # pip install git+https://github.com/statsmodels/statsmodels
-
-MODEL = 'ordinal' #'multinomial'
+MODEL = 'ordinal'  # 'multinomial'
 
 
 def main():
@@ -34,19 +35,69 @@ def main():
 
     print("\nMultinomial Logistic Regression . . .")
 
-    X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X_new, Y, test_size=0.20, random_state=5)
-
+    X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X_new, Y, test_size=0.20,
+                                                                                random_state=5)
+    # Using ordinal logistic regression
     if MODEL == 'ordinal':
-        X_train = pd.DataFrame(X_train, dtype="object")
-        y_train = pd.DataFrame(y_train, dtype="category")
+        '''
+        # Put y_train in correct datatype
+        y_train = y_train.to_numpy()
+        for i in range(0, y_train.shape[0]):
+            if y_train[i] == '[1.0, 0.0, 0.0, 0.0]':
+                y_train[i] = 'a'
+            if y_train[i] == '[0.0, 1.0, 0.0, 0.0]':
+                y_train[i] = 'b'
+            if y_train[i] == '[0.0, 0.0, 1.0, 0.0]':
+                y_train[i] = 'c'
+            if y_train[i] == '[0.0, 0.0, 0.0, 1.0]':
+                y_train[i] = 'd'
+        y_cat = pd.Categorical(y_train, categories=["a", "b", "c", "d"], ordered=True)
+        y_train = pd.Series(y_cat)
 
+        # Put X_train in correct datatype
+        X_train = pd.DataFrame(X_train)
+
+        # Create the model
         ordinalModel = OrderedModel(endog=y_train, exog=X_train, distr='logit')
         res_log = ordinalModel.fit(method='bfgs', disp=True)
         res_log.summary()
-        y_pred = res_log.predict(X_test)
-        print('Accuracy: {:.2f}'.format(accuracy_score(y_test, y_pred)))
-        print('Error rate: {:.2f}'.format(1 - accuracy_score(y_test, y_pred)))
 
+        # Determine correctness
+        predicted = res_log.model.predict(res_log.params, exog=X_train)
+        pred_choice = predicted.argmax(1)
+        print('Fraction of correct choice predictions')
+        print((np.asarray(y_train.values.codes) == pred_choice).mean())
+        '''
+
+        # Put y_train in correct datatype
+        Y = Y.to_numpy()
+        for i in range(0, Y.shape[0]):
+            if Y[i] == '[1.0, 0.0, 0.0, 0.0]':
+                Y[i] = 'a'
+            if Y[i] == '[0.0, 1.0, 0.0, 0.0]':
+                Y[i] = 'b'
+            if Y[i] == '[0.0, 0.0, 1.0, 0.0]':
+                Y[i] = 'c'
+            if Y[i] == '[0.0, 0.0, 0.0, 1.0]':
+                Y[i] = 'd'
+        Y_cat = pd.Categorical(Y, categories=["a", "b", "c", "d"], ordered=True)
+        Y = pd.Series(Y_cat)
+
+        # Put X_train in correct datatype
+        X_new = pd.DataFrame(X_new)
+
+        # Create the model
+        ordinalModel = OrderedModel(endog=Y, exog=X_new, distr='logit')
+        res_log = ordinalModel.fit(method='bfgs', disp=True)
+        res_log.summary()
+
+        # Determine correctness
+        predicted = res_log.model.predict(res_log.params, exog=X_new)
+        pred_choice = predicted.argmax(1)
+        print('Fraction of correct choice predictions')
+        print((np.asarray(Y.values.codes) == pred_choice).mean())
+
+    # Using multinomial logistic regression
     if MODEL == 'multinomial':
         model = LogisticRegression(multi_class='multinomial', solver='lbfgs', max_iter=1500)
         model.fit(X_train, y_train)
